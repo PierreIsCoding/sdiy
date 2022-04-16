ae8cc57537247bdbd6c1ffba736c458fead38d20
PierreIsCoding <34983245+PierreIsCoding@users.noreply.github.com>
2022-04-16 11:27:19 +0200

    Add files via upload

---
+++ Attiny_Programmer/CODE/TEST_BLINK/TEST_BLINK.ino
@@ -0,0 +1,15 @@
+
+int led = 3;
+
+
+void setup() {
+  pinMode(led, OUTPUT);
+}
+
+
+void loop() {
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(1000);               // wait for a second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(1000);               // wait for a second
+}
